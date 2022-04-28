9c9486e40b2eb274c3dc32757e707d99c07a589c
ianclarke03 <83726782+ianclarke03@users.noreply.github.com>
2022-04-28 19:00:15 -0400

    Create more-bad-code.cpp

---
+++ more-bad-code.cpp
@@ -0,0 +1,10 @@
+
+                            int main(){
+                        //Hello, World!
+                     // Hi, I'm a program!
+                  int y = 1; 
+              for(int z = 0; z < 10; z++) {
+          cout << z;
+      cout << endl;
+   }
+ }

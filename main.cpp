edcd119a5cbf6b4354c06c3ffe590432ebf5b595
mshvorin <71457948+mshvorin@users.noreply.github.com>
2022-04-29 02:01:58 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,19 +9,15 @@ int main()
 {
   std::ifstream fin;
 
-  
   if(fin.fail()){ 
         std::cerr << "File cannot be opened"; 
         exit(1);
   }
-
   
   std::string line;
-  
   int out_brace = 0;
   int in_brace = 0;
   fin.open("bad-code.cpp");
-
 
   while(getline(fin, line)) { 
         in_brace = countChar(line, '}') + in_brace;
@@ -32,9 +28,7 @@ int main()
   
   fin.close();
 
-
   std::string line2;
-  
   out_brace = 0;
   in_brace = 0;
   fin.open("bad-code2.cpp");
